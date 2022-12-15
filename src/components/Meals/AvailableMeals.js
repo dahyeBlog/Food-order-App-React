c6188be9d66b0d48fe8a1b963c8457e0711ffaf9
dahye <dhk8233@gmail.com>
2022-12-15 22:00:02 +0900

    foodOrderApp

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,63 +1,60 @@
-import React,{useState, useEffect} from 'react'
-import classes from "./AvailableMeals.module.css"
-import MealItem from './MealItem/MealItem'
+import React, { useState, useEffect } from "react";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([])
+  const [meals, setMeals] = useState([]);
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch ('https://food-app-react-bf217-default-rtdb.firebaseio.com/menu.json')
+      const response = await fetch(
+        "https://food-app-react-bf217-default-rtdb.firebaseio.com/menu.json"
+      );
 
-      if(!response.ok) {
-        throw new Error('에러가 발생했습니다.')
+      if (!response.ok) {
+        throw new Error("에러가 발생했습니다.");
       }
 
-      const responseData = await response.json()
+      const responseData = await response.json();
 
-      const loadedMeals = []
+      const loadedMeals = [];
 
-      for(const key in responseData) {
+      for (const key in responseData) {
         loadedMeals.push({
-          id:key,
+          id: key,
           name: responseData[key].name,
           description: responseData[key].des,
           price: responseData[key].price,
-          image: responseData[key].image
-        })
+          image: responseData[key].image,
+        });
       }
 
-      setMeals(loadedMeals)
-
-    }
+      setMeals(loadedMeals);
+    };
 
     fetchMeals().catch((error) => {
       console.log(error);
-      
-    })
-
-  }, [])
-
+    });
+  }, []);
 
   const mealsList = meals.map((meal) => {
-  return <MealItem 
-    id={meal.id}
-    key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}  
-    image={meal.image}/>  
-    })
-
-
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+        image={meal.image}
+      />
+    );
+  });
 
   return (
     <section>
-      <div className={classes.mealLists}>
-        <ul>{mealsList}</ul>
-      </div>
+      <div className={classes.mealLists}>{mealsList}</div>
     </section>
-  )
-}
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,37 +1,39 @@
-import React,{useContext} from 'react'
-import MealItemForm from './MealItemForm'
-import CartContext from '../../../store/cart-context'
+import React, { useContext } from "react";
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+import classes from "./MealItem.module.css";
 
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
 
-const MealItem = (props) => {    
-  const cartCtx = useContext(CartContext)
+  const price = `${props.price
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`;
 
-  const price = `${props.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`
-
-  
   const addToCartHandler = (amount) => {
-    cartCtx.addItem({ 
+    cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price,})
-    
-    
-  } 
-
-
+      price: props.price,
+    });
+  };
 
   return (
-    <li>
-      <div>
-        <h3>{props.name}</h3>
-        <div>{props.description}</div>
-        <div>{price}</div>
-        <img src={props.image} alt={props.name} />
-        <MealItemForm onAddToCart={addToCartHandler} />
-      </div>
-    </li>
-  )
-}
+    <div className={classes.mealItem}>
+      <ul>
+        <li>
+          <h3>{props.name}</h3>
+          <div className={classes.mealDec}>{props.description}</div>
+          <div className={classes.price}>{price}</div>
+          <div className={classes.mealImg}>
+          <img src={props.image} alt={props.name} />
+          </div>
+          <MealItemForm onAddToCart={addToCartHandler} />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default MealItem
+export default MealItem;
